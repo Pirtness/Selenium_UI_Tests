29c1be2f2624d947232f3a1d91d7891e1b65035a
Pirtness <edelal60@mail.ru>
2021-04-09 10:44:19 +0300

    test go to login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,8 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url 
 
     def should_be_login_form(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
--- test_login_page.py
+++ test_login_page.py
@@ -1,12 +1,5 @@
 from .pages.login_page import LoginPage
 
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)
